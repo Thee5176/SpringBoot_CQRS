3156309ef7e652d38fefc0ef081d41056cc3893c
Theerapong Likhitsab <Theerapong5176@gmail.com>
2025-08-19 14:22:16 +0900

    resolve aws keypair

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -12,20 +12,14 @@ data "aws_ami" "ubuntu" {
   }
 }
 
-resource "tls_private_key" "ec2_ssh_key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "aws_key_pair" "ec2_key_pair" {
-  key_name   = "terraform-generated-key"
-  public_key = tls_private_key.ec2_ssh_key.public_key_openssh
+data "aws_key_pair" "deployment_key" {
+  key_name = "github_workflow_key" # Manually created on aws console
 }
 
 resource "aws_instance" "web_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
-  key_name      = aws_key_pair.ec2_key_pair.key_name
+  key_name      = data.aws_key_pair.deployment_key.key_name
 
   tags = {
     Name = "web-server"
--- envs/dev/output.tf
+++ envs/dev/output.tf
@@ -1,9 +1,3 @@
-resource "local_file" "ssh_private_key_pem" {
-  content         = tls_private_key.ec2_ssh_key.private_key_pem
-  filename        = "tf_generated_key.pem" # This file will be created in the current directory
-  file_permission = "0600"                 # Set permissions for SSH
-}
-
 output "instance_public_ip" {
   description = "Public IP address of the EC2 instance."
   value       = aws_instance.web_server.public_ip
