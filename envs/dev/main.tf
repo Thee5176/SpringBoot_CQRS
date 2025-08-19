0462f5ad62d3cae11f68f6414c599d566cfdc85d
Theerapong Likhitsab <Theerapong5176@gmail.com>
2025-08-19 11:20:15 +0900

    add terraform and deploy workflow

---
+++ envs/dev/main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region = "ap-northeast-1" #japan
+
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+}
+
+resource "aws_key_pair" "ec2_key_pair" {
+  key_name   = "ssh_deploy_key"
+  public_key = env("~/.ssh/my_ec2_key.pub")
+}
+
+resource "aws_instance" "web_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  key_name = resource.aws_key_pair.ec2_key_pair.key_name
+
+  tags = {
+    Name = "web-server"
+  }
+}
