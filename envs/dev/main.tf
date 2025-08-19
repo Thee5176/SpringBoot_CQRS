2ac3ea779c951329d53ec724bcb7fb44ee4e14de
Theerapong Likhitsab <Theerapong5176@gmail.com>
2025-08-20 08:14:58 +0900

    devide CI/CD workflow

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -17,7 +17,7 @@ resource "aws_security_group" "web_server_sg" {
   ingress {
     description = "HTTP from anywhere"
     from_port   = 80
-    to_port     = 80
+    to_port     = 8083
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
