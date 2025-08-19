f8373fea61b406b9aaa2df7b72a1efacf8d6043d
Theerapong Likhitsab <Theerapong5176@gmail.com>
2025-08-19 17:12:16 +0900

    test CI

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -12,6 +12,38 @@ data "aws_ami" "ubuntu" {
   }
 }
 
+resource "aws_security_group" "web_server_sg" {
+  name        = "web-server-sg"
+  description = "Allow SSH and HTTP inbound traffic"
+
+  ingress {
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allows SSH from any IP address
+  }
+
+  ingress {
+    description = "HTTP from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-server-sg"
+  }
+}
+
 data "aws_key_pair" "deployment_key" {
   key_name = "github_workflow_key" # Manually created on aws console
 }
